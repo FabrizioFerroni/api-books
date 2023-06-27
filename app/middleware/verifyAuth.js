34d4a559f1b01d2a2e51ee90174f7532446bd1c9
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2023-06-27 07:26:41 -0300

    Se cambio los mensajes de ingles a español

--- app/middleware/verifyAuth.js
+++ app/middleware/verifyAuth.js
@@ -7,29 +7,29 @@ const secret = process.env.JWT_SECRET || "";
 export function validateLogin(req, res, next) {
     const { authorization } = req.headers;
 
-    if (!authorization) return res.status(403).send({ message: `You don't have sent a token` });
+    if (!authorization) return res.status(403).send({ message: `No has enviado un token.` });
 
     let token = authorization.replace(/['"]+/g, '');
 
     let seg = token.split('.');
 
     if (seg.length != 3) {
-        return res.status(403).send({ message: 'Invalid Token' });
+        return res.status(403).send({ message: 'Token invalido' });
     } else {
         try {
             const payload = Jwt.decode(token, secret);
             if (payload.exp <= moment().unix()) {
-                return res.status(403).send({ message: 'Expired Token' });
+                return res.status(403).send({ message: 'Token expirado' });
             }
         } catch (error) {
-            return res.status(403).send({ message: 'Invalid Token' });
+            return res.status(403).send({ message: 'Token invalido' });
         }
     }
 
     Jwt.verify(token, secret, (err, decoded) => {
         if (err) {
             return res.status(401).send({
-                message: "Unauthorized!"
+                message: "No estas autorizado para hacer esta operacion!"
             });
         }
         req.userId = decoded.id;
